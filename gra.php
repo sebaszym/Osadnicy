59b705d20757132a23e21f3d95c0c2fa89b8f195
sebaszym <sebaszym@sebaszym.pl>
2016-01-17 17:37:41 +0100

    Osadnicy

--- gra.php
+++ gra.php
@@ -21,7 +21,7 @@
 
 
 
-	
+<body>	
 <?php
     $zostalodnipremium;
 	$dnipremium = $_SESSION['dnipremium'];
@@ -35,15 +35,6 @@
 	echo "<p><b>E-mail</b>: ".$_SESSION['email'];
 	echo "<br /><b>Vip</b>: ".$zostalodnipremium." dni</p>";
 ?>
-<body>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="SXURWBVXXC85G">
-<input type="image" src="https://www.paypalobjects.com/pl_PL/PL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – Płać wygodnie i bezpiecznie">
-<img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1">
-</form>
-<div>Lub wyślij smsa pod numer 73068 o treści TC.TSP3.665177.[wiadomość]<br />Na przykład: TC.TSP3.665177.Bardzo fajna gra!</div>
-<div>A to są wykonane dotacje sms:</div>
-<div id="pwebLastPayments" class="PGRpdiBkYXRhLXBhcmFtcz0iP3A9Tlh3Mk5qVXhOemM9JmFjdGlvbkxhc3Q9ZCIgaWQ9InB3ZWJUYWJsZUxhc3REb25hdGUiPjwvZGl2Pg=="></div>
+
 </body>
 </html>
